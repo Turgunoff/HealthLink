e47a1f5dae14236579bb35710545b933b35a00f6
Eldor <eldorturgunov777@gmail.com>
2024-10-04 09:59:13 +0500

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,18 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:health_link/app/app.dart';
-import 'package:health_link/app/config/theme.dart';
 import 'package:health_link/app/config/theme/dark_theme.dart';
 import 'package:health_link/app/config/theme/light_theme.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 import 'features/home/logic/home_bloc.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   // await Hive.initFlutter(); // Hive'ni ishga tushiramiz
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       systemNavigationBarColor: Colors.transparent,
